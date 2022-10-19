5740cd45e70e8a0ecb2dd83738af054cd4b2e497
LuizAdamchuk <adamchuk.junior@gmail.com>
2022-10-19 18:26:41 -0300

    fix: Visual bugs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -32,7 +32,8 @@ function App() {
       <h1 className="text-6xl text-white font-black mt-20">
         Seu
         <span className="text-transparent bg-nlw-gradient bg-clip-text">
-          duo
+          {" "}
+          duo{" "}
         </span>
         está aqui.
       </h1>
